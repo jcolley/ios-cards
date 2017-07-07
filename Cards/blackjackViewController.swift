724311236b55fc75997fee6882a3deffc1bc2b7c
John Colley <webjocky@gmail.com>
2017-07-06 18:23:57 -0700

    Game Init function complete

--- Cards/blackjackViewController.swift
+++ Cards/blackjackViewController.swift
@@ -14,21 +14,24 @@ class blackjackViewController: UIViewController {
     @IBOutlet weak var playerHandValue1: UILabel!
     @IBOutlet weak var playerHandSuit0: UIImageView!
     @IBOutlet weak var playerHandValue0: UILabel!
+    
     @IBOutlet weak var dealerHandSuit1: UIImageView!
     @IBOutlet weak var dealerHandValue1: UILabel!
     @IBOutlet weak var dealerHandCardback1: UIImageView!
     @IBOutlet weak var dealerHandSuit0: UIImageView!
     @IBOutlet weak var dealerHandValue0: UILabel!
     @IBOutlet weak var dealerHandCardback0: UIImageView!
+    
     @IBOutlet weak var playDeckBack: UIImageView!
     @IBOutlet weak var playDeckCard: UIImageView!
+    
+    @IBOutlet weak var hitPileCard0: UIImageView!
+    @IBOutlet weak var hitPileCardback0: UIImageView!
     @IBOutlet weak var hitPileValue0: UILabel!
     @IBOutlet weak var hitPileSuit0: UIImageView!
-    @IBOutlet weak var hitPileCard0: UIImageView!
     @IBOutlet weak var hitPileValue1: UILabel!
     @IBOutlet weak var hitPileSuit1: UIImageView!
     @IBOutlet weak var hitPileCardback1: UIImageView!
-    @IBOutlet weak var hitPileCardback0: UIImageView!
     @IBOutlet weak var hitPileCard1: UIImageView!
     @IBOutlet weak var hitPileSuit2: UIImageView!
     @IBOutlet weak var hitPileCardback2: UIImageView!
@@ -72,11 +75,16 @@ class blackjackViewController: UIViewController {
         
     }
     
+    //Global vars
+    var deck = Deck()
+    var playerHand = [Card]()
+    var dealerHand = [Card]()
+    var cardPlay = ViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        playerHandSuit1.isHidden = false
         
-        //hideHitPile()
+        initGame()
         
         // Do any additional setup after loading the view.
     }
@@ -86,37 +94,59 @@ class blackjackViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func initGame() {
+        playerHand.append(deck.deal())
+        dealerHand.append(deck.deal())
+        playerHand.append(deck.deal())
+        dealerHand.append(deck.deal())
 
-    func hideHitPile() {
-        hitPileValue0.isHidden = false
-        hitPileSuit0.isHidden = false
-        hitPileCard0.isHidden = false
-        hitPileValue1.isHidden = false
-        hitPileSuit1.isHidden = false
-        hitPileCardback1.isHidden = false
-        hitPileCardback0.isHidden = false
-        hitPileCard1.isHidden = false
-        hitPileSuit2.isHidden = false
-        hitPileCardback2.isHidden = false
-        hitPileCard2.isHidden = false
-        hitPileValue2.isHidden = false
-        hitPileSuit3.isHidden = false
-        hitPileValue3.isHidden = false
-        hitPileCardback3.isHidden = false
-        hitPileCard3.isHidden = false
-        hitPileValue4.isHidden = false
-        hitPileSuit4.isHidden = false
-        hitPileCardback4.isHidden = false
-        hitPileCard4.isHidden = false
-        hitPileValue5.isHidden = false
-        hitPileSuit5.isHidden = false
-        hitPileCardback5.isHidden = false
-        hitPileCard5.isHidden = false
-        hitPileValue6.isHidden = false
-        hitPileSuit6.isHidden = false
-        hitPileCardback6.isHidden = false
-        hitPileCard6.isHidden = false
+        playerHandValue0.text = String(playerHand[0].numerical_value)
+        playerHandSuit0.image = cardPlay.getSuitImage(suit: playerHand[0].suit)
+        playerHandValue1.text = String(playerHand[1].numerical_value)
+        playerHandSuit1.image = cardPlay.getSuitImage(suit: playerHand[1].suit)
+        
+        dealerHandValue0.text = String(dealerHand[0].numerical_value)
+        dealerHandSuit0.image = cardPlay.getSuitImage(suit: dealerHand[0].suit)
+        dealerHandValue1.text = String(dealerHand[1].numerical_value)
+        dealerHandSuit1.image = cardPlay.getSuitImage(suit: dealerHand[1].suit)
+        
+        dealerHandCardback0.isHidden = false
+        dealerHandCardback1.isHidden = false
+        dealerHandSuit0.isHidden = true
+        dealerHandSuit1.isHidden = true
+        dealerHandValue0.isHidden = true
+        dealerHandValue0.isHidden = true
+        
+        hitPileValue0.isHidden = true
+        hitPileSuit0.isHidden = true
+        hitPileCard0.isHidden = true
+        hitPileValue1.isHidden = true
+        hitPileSuit1.isHidden = true
+        hitPileCardback1.isHidden = true
+        hitPileCardback0.isHidden = true
+        hitPileCard1.isHidden = true
+        hitPileSuit2.isHidden = true
+        hitPileCardback2.isHidden = true
+        hitPileCard2.isHidden = true
+        hitPileValue2.isHidden = true
+        hitPileSuit3.isHidden = true
+        hitPileValue3.isHidden = true
+        hitPileCardback3.isHidden = true
+        hitPileCard3.isHidden = true
+        hitPileValue4.isHidden = true
+        hitPileSuit4.isHidden = true
+        hitPileCardback4.isHidden = true
+        hitPileCard4.isHidden = true
+        hitPileValue5.isHidden = true
+        hitPileSuit5.isHidden = true
+        hitPileCardback5.isHidden = true
+        hitPileCard5.isHidden = true
+        hitPileValue6.isHidden = true
+        hitPileSuit6.isHidden = true
+        hitPileCardback6.isHidden = true
+        hitPileCard6.isHidden = true
     }
+    
     /*
     // MARK: - Navigation
 
